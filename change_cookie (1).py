b59ac1648a3ce32f05a0dfb43b481a80ccf902b4
RaiXshenzu <153984170+ShinDevv@users.noreply.github.com>
2025-05-04 20:27:13 +0800

    Add files via upload

---
+++ change_cookie (1).py
@@ -0,0 +1,11 @@
+def get_cookies():
+  cookies = {
+      "_ga_57E30E1PMN": "GS1.1.1744810655.4.1.1744810691.0.0.0",
+      "_ga": "GA1.1.1885656084.1744648259",
+      "_ga_XB5PSHEQB4": "GS1.1.1743783442.1.0.1743783450.0.0.0",
+      "token_session": "c73c001a5df1e2ff6487f23a32d1d8985ebec80406f5a8ba7f18e822df377891ff4d0873198bf9ab6a7757b9cc353a72",
+      "_ga_G8QGMJPWWV": "GS1.1.1743783451.3.0.1743783456.0.0.0",
+      "_ga_1M7M9L6VPX": "GS1.1.1743946750.5.1.1743946758.0.0.0",
+      "datadome": "CoqFe~L1NCz0N1zPYovgaVOs2_kT859EV9SgJBXhuC2ciCxMj636d35Jm~UrtDo6aDR0hul6840cNL4UFjZfRriuSeTcuXuMuj3WyRVuFWAwWGL_jBcOia3~Jaq_jabp"
+  }
+  return cookies
